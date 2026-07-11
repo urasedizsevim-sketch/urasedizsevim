493afbf60463e25e97fb44986401ed07561a94a9
urasedizsevim-sketch <urasedizsevim@gmail.com>
2026-07-12 02:10:26 +0300

    Update encode.py

--- encode.py
+++ encode.py
@@ -1,11 +1,4 @@
-import time
 import random
-
-def s(saniye):
-    time.sleep(saniye)
-    return 0
-
-metin = input("UESLocker(encoder): ").upper().replace("i", "İ").replace("ı", "I")
 
 listA = {
     "A": 1, "B": 2, "C": 3, "Ç": 4, "D": 5, "E": 6, "F": 7, "G": 8, "Ğ": 9,
@@ -18,28 +11,24 @@ listA = {
     "}": 58, "~": 59,
 }
 
-print("encoding...")
-for _ in range(5):
-    s(0.1)
+def ues_sifrele(metin):
+    metin = metin.upper().replace("i", "İ").replace("ı", "I")
+    
+    tuz_anahtari = random.randint(100, 999)
+    sifreli_liste = []
+    
+    for sira, harf in enumerate(metin):
+        if harf in listA:
+            orijinal_deger = listA[harf]
+            karmasik_sayi = (orijinal_deger * (sira + 1)) + tuz_anahtari + (orijinal_deger ** 2)
+            sifreli_liste.append(str(karmasik_sayi))
 
-tuz_anahtari = random.randint(100, 999)
+    sifreli_metin = "x".join(sifreli_liste)
 
-sifreli_liste = []
-for sira, harf in enumerate(metin):
-    if harf in listA:
-        orijinal_deger = listA[harf]
-
-        karmasik_sayi = (orijinal_deger * (sira + 1)) + tuz_anahtari + (orijinal_deger ** 2)
-        sifreli_liste.append(str(karmasik_sayi))
-
-sifreli_metin = "x".join(sifreli_liste)
-
-dosya = open("history.txt", "a", encoding="utf-8")
-dosya.write("\n\nmetin:\n" + metin)
-dosya.write("\nşifreli metin:\n" + sifreli_metin)
-dosya.write("\nkey'i:\n" + str(tuz_anahtari))
-dosya.close()
-print("\n" + "="*40)
-print("UESLocker: ", sifreli_metin)
-print("UESLocker(key): ", tuz_anahtari)
-print("="*40)
+    dosya = open("history.txt", "a", encoding="utf-8")
+    dosya.write("\n\nmetin:\n" + metin)
+    dosya.write("\nşifreli metin:\n" + sifreli_metin)
+    dosya.write("\nkey'i:\n" + str(tuz_anahtari))
+    dosya.close()
+    
+    return sifreli_metin, tuz_anahtari
