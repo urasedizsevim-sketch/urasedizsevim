558c04203883c53b77f0234de2b37c97d5b26575
urasedizsevim-sketch <urasedizsevim@gmail.com>
2026-07-12 01:57:55 +0300

    Add files via upload

---
+++ desifre.py
@@ -0,0 +1,46 @@
+import time
+
+
+def s(saniye):
+    time.sleep(saniye)
+    return 0
+
+
+sifreli_girdi = input("UESLocker(decoder): ").strip()
+tuz_anahtari = int(input("UESLocker(key): "))
+
+ters_listA = {
+    1: "A", 2: "B", 3: "C", 4: "Ç", 5: "D", 6: "E", 7: "F", 8: "G", 9: "Ğ",
+    10: "H", 11: "I", 12: "İ", 13: "J", 14: "K", 15: "L", 16: "M", 17: "N",
+    18: "O", 19: "Ö", 20: "P", 21: "R", 22: "S", 23: "Ş", 24: "T", 25: "U",
+    26: "Ü", 27: "V", 28: "Y", 29: "Z"
+}
+
+print("decoder...")
+for _ in range(5):
+    s(0.1)
+
+parcalar = sifreli_girdi.split("x")
+cozulen_harfler = []
+
+for sira, parca in enumerate(parcalar):
+    if parca.isdigit():
+        karmasik_sayi = int(parca)
+
+        bulundu = False
+        for muhtemel_deger in range(1, 30):
+            test_sayisi = (muhtemel_deger * (sira + 1)) + tuz_anahtari + (muhtemel_deger ** 2)
+
+            if test_sayisi == karmasik_sayi:
+                if muhtemel_deger in ters_listA:
+                    cozulen_harfler.append(ters_listA[muhtemel_deger])
+                    bulundu = True
+                    break
+
+        if not bulundu:
+            cozulen_harfler.append("?")
+
+orijinal_metin = "".join(cozulen_harfler)
+print("\n" + "=" * 40)
+print("UESLocker: ", orijinal_metin)
+print("=" * 40)
---
+++ encode.py
@@ -0,0 +1,45 @@
+import time
+import random
+
+def s(saniye):
+    time.sleep(saniye)
+    return 0
+
+metin = input("UESLocker(encoder): ").upper().replace("i", "İ").replace("ı", "I")
+
+listA = {
+    "A": 1, "B": 2, "C": 3, "Ç": 4, "D": 5, "E": 6, "F": 7, "G": 8, "Ğ": 9,
+    "H": 10, "I": 11, "İ": 12, "J": 13, "K": 14, "L": 15, "M": 16, "N": 17,
+    "O": 18, "Ö": 19, "P": 20, "R": 21, "S": 22, "Ş": 23, "T": 24, "U": 25,
+    "Ü": 26, "V": 27, "Y": 28, "Z": 29, " ": 30, "!": 31, "'": 32, "^": 33,
+    "+": 34, "%": 35, "&": 36, "/": 37, "(": 38, ")": 39, "=": 40, "*": 41,
+    "?": 42, "-": 43, "_": 44, "|": 45, ",": 46, ".": 47, ":": 48, ";": 49,
+    "@": 50, "<": 51, ">": 52, "#": 53, "$": 54, "{": 55, "[": 56, "]": 57,
+    "}": 58, "~": 59,
+}
+
+print("encoding...")
+for _ in range(5):
+    s(0.1)
+
+tuz_anahtari = random.randint(100, 999)
+
+sifreli_liste = []
+for sira, harf in enumerate(metin):
+    if harf in listA:
+        orijinal_deger = listA[harf]
+
+        karmasik_sayi = (orijinal_deger * (sira + 1)) + tuz_anahtari + (orijinal_deger ** 2)
+        sifreli_liste.append(str(karmasik_sayi))
+
+sifreli_metin = "x".join(sifreli_liste)
+
+dosya = open("history.txt", "a", encoding="utf-8")
+dosya.write("\n\nmetin:\n" + metin)
+dosya.write("\nşifreli metin:\n" + sifreli_metin)
+dosya.write("\nkey'i:\n" + str(tuz_anahtari))
+dosya.close()
+print("\n" + "="*40)
+print("UESLocker: ", sifreli_metin)
+print("UESLocker(key): ", tuz_anahtari)
+print("="*40)
